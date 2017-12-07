33f4d5687a5f28540d3f8cf296a7188981cc8123
janto <ja.merino@usp.ceu.es>
2017-12-07 15:12:34 +0100

    añadi un nivel de verificacion, si el user y password son ambos ram podras acceder al contenido de intermediario y posteriormente acceder a servletDestino, sino accedes se te redireccionara al index.html

--- src/java/Filter/Intermediario.java
+++ src/java/Filter/Intermediario.java
@@ -30,22 +30,27 @@ public class Intermediario extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("ESTAS INTERMEDIARIO");
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+       
+        PrintWriter out = response.getWriter();
+           
+        if (!(user.equalsIgnoreCase("ram") && password.equals("ram")))
+        {
+               System.out.println("Unauthorized request");
+            System.out.println("######################################################\n\n");
+            response.sendRedirect("index.html");
+        }
+        else
+        {
+             out.println("ESTAS INTERMEDIARIO");
             out.println("<br>");
             out.println("  <a href=\"/filter2/ServletDestino\">Ir a Destino</a>");
-            
-            out.println("<title>Servlet Intermediario</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Intermediario at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
+
+    
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
